48956043da3e15315fc0902a07435c3b0872c5e8
YooDang <A20213279@HackerLogin.8.8.8.8>
2024-03-25 20:34:08 +0900

    solve 2557

--- baekjoon/1001/1001.c
+++ baekjoon/1001/1001.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
-int main(void){
-    printf("Hello World");
+int main(void)
+{
+    printf("int 자료형의 크기: %ld바이트\n", sizeof(int));
+    printf("float 자료형의 크기: %ld바이트\n", sizeof(float));
+    printf("char 자료형의 크기: %ld바이트\n", sizeof(char));
 }
